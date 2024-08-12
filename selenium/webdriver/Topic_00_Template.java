b028a954dc3b317eae772333ab2ccd1fb07d4d9c
Nhi Doan <ynhi293@gmail.com>
2024-08-12 18:19:42 +0700

    Add topic 02

---
+++ selenium/webdriver/Topic_00_Template.java
@@ -0,0 +1,28 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Topic_00_Template {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @Test
+    public void TC_01_() {
+
+    }
+
+    @Test
+    public void TC_02_() {
+
+    }
+
+    @Test
+    public void TC_03_() {
+
+    }
+}
