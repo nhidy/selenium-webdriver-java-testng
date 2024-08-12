b0d8f95c7be8c5ba18879329f10aa67b9958f3de
Đoàn Ý Nhi <nhidy@jits.vn>
2024-08-12 08:52:26 +0700

    Initial commit

---
+++ selenium/javaTester/TestSystemInfo.java
@@ -0,0 +1,8 @@
+package javaTester;
+
+public class TestSystemInfo {
+    public static void main(String[] args) {
+        String osName = System.getProperty("os.name");
+        System.out.println(osName);
+    }
+}
