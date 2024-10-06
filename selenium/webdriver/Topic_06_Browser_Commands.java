d1f498fcadaf1e640bcf50b2a605044cb4920411
Nhi Doan <ynhi293@gmail.com>
2024-10-06 11:44:21 +0700

    Update exercise topic 06 browser commands

--- selenium/webdriver/Topic_06_Browser_Commands.java
+++ selenium/webdriver/Topic_06_Browser_Commands.java
@@ -1,7 +1,9 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -21,25 +23,53 @@ public class Topic_06_Browser_Commands {
     @Test
     public void TC_01_Verify_Url() {
         driver.get("http://live.techpanda.org/");
-
+        driver.findElement(By.xpath("//div[@class='footer-container']//a[@title='My Account']")).click();
+        sleepInSeconds(2);
+        // Verify
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+        sleepInSeconds(2);
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
     }
 
     @Test
     public void TC_02_Verify_Title() {
-        driver.get("");
-
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer-container']//a[@title='My Account']")).click();
+        sleepInSeconds(2);
+        // Verify
+        Assert.assertEquals(driver.getTitle(), "Customer Login");
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+        sleepInSeconds(2);
+        Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
     }
 
     @Test
     public void TC_03_Navigate_Function() {
-        driver.get("");
-
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer-container']//a[@title='My Account']")).click();
+        sleepInSeconds(2);
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+        // Verify
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+        driver.navigate().back();
+        sleepInSeconds(2);
+        Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
+        driver.navigate().forward();
+        sleepInSeconds(2);
+        Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
     }
 
     @Test
     public void TC_04_Get_Page_Source_Code() {
-        driver.get("");
-
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer-container']//a[@title='My Account']")).click();
+        sleepInSeconds(2);
+        // Verify
+        Assert.assertTrue(driver.getPageSource().contains("Login or Create an Account"));
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+        sleepInSeconds(2);
+        Assert.assertTrue(driver.getPageSource().contains("Create an Account"));
     }
 
     public void sleepInSeconds(long timeInSecond) {
