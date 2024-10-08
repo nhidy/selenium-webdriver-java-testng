9ca42d2934f7c5cba6a2d6650472b309d7431c1a
Nhi Doan <ynhi293@gmail.com>
2024-10-08 22:59:49 +0700

    Update exercise topic 06 element commands

--- selenium/webdriver/Topic_06_Login.java
+++ selenium/webdriver/Topic_06_Login.java
@@ -1,12 +1,15 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.Random;
 
 public class Topic_06_Login {
     WebDriver driver;
@@ -16,27 +19,49 @@ public class Topic_06_Login {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.get("");
     }
 
     @Test
     public void TC_01_Login_With_Empty_Email_And_Password() {
-
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer-container']//a[@title='My Account']")).click();
+        sleepInSeconds(2);
+        driver.findElement(By.cssSelector("button#send2")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-required-entry-email")).getText(), "This is a required field.");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-required-entry-pass")).getText(), "This is a required field.");
     }
 
     @Test
     public void TC_02_Login_With_Invalid_Email() {
-
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer-container']//a[@title='My Account']")).click();
+        sleepInSeconds(2);
+        driver.findElement(By.cssSelector("input#email")).sendKeys("123456@123456");
+        driver.findElement(By.cssSelector("input#pass")).sendKeys("12345678");
+        driver.findElement(By.cssSelector("button#send2")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-validate-email-email")).getText(), "Please enter a valid email address. For example johndoe@domain.com.");
     }
 
     @Test
     public void TC_03_Login_With_Password_Invalid() {
-
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer-container']//a[@title='My Account']")).click();
+        sleepInSeconds(2);
+        driver.findElement(By.cssSelector("input#email")).sendKeys("automation@gmail.net");
+        driver.findElement(By.cssSelector("input#pass")).sendKeys("123");
+        driver.findElement(By.cssSelector("button#send2")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-validate-password-pass")).getText(), "Please enter 6 or more characters without leading or trailing spaces.");
     }
 
     @Test
     public void TC_04_Login_With_Incorrect_Email_Password() {
-
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer-container']//a[@title='My Account']")).click();
+        sleepInSeconds(2);
+        driver.findElement(By.cssSelector("input#email")).sendKeys("automation@gmail.net");
+        driver.findElement(By.cssSelector("input#pass")).sendKeys("123456");
+        driver.findElement(By.cssSelector("button#send2")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("li.error-msg >ul >li >span")).getText(), "Invalid login or password.");
     }
 
     public void sleepInSeconds(long timeInSecond) {
@@ -45,6 +70,11 @@ public class Topic_06_Login {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public String getEmailAddress (){
+        Random rand = new Random();
+        return "automation" + rand.nextInt(9999) + "@gmail.net";
     }
 
     @AfterClass
