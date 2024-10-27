f31c50953e521d014277a86583ff421c285b5b23
Nhi Doan <ynhi293@gmail.com>
2024-10-27 22:03:02 +0700

    Update name part 03 topic 08

--- selenium/webdriver/Part03_Topic_08_Custom_Dropdown.java
+++ selenium/webdriver/Part03_Topic_08_Custom_Dropdown.java
@@ -1,44 +1,48 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Random;
 
-public class Topic_00_Template {
+public class Part03_Topic_08_Custom_Dropdown {
     WebDriver driver;
-    String projectPath = System.getProperty("user.dir");
-    String osName = System.getProperty("os.name");
+    WebDriverWait explicitWait;
 
     @BeforeClass
     public void beforeClass () {
         driver = new FirefoxDriver();
-        driver = new ChromeDriver();
-        driver = new EdgeDriver();
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(30));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.get("");
     }
 
     @Test
-    public void TC_01_() {
+    public void TC_01_Click_Items() {
+        driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
+        driver.findElement(By.cssSelector("span#number-button")).click();
+        explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("ul#number-menu div")));
+        List<WebElement> allItems = driver.findElements(By.cssSelector("ul#number-menu div"));
+        for (WebElement item : allItems) {
+            String textItem = item.getText();
+            System.out.println("Text item = " + textItem);
 
-    }
-
-    @Test
-    public void TC_02_() {
-
-    }
-
-    @Test
-    public void TC_03_() {
-
+            if (textItem.equals("15")){
+                item.click();
+                break;
+            }
+        }
     }
 
     @AfterClass
